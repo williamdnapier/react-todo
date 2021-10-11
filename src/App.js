86de5869db9fbce324b0a45e3f6ef4a078d3c74c
William D. Napier <william.napier@uspsector.com>
2021-10-11 09:44:37 -0400

    add files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h4 className='bg-primary text-white text-center p-2'>To Do List</h4>
+      </div>
+    )
+  }
+}
